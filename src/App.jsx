29a431c8804d3c0198cf84edf0b87dc1fea20621
nilo <nil.lara.tan@hotmail.com>
2023-06-11 23:01:34 +0300

    manitasız 11.gün

--- src/App.jsx
+++ src/App.jsx
@@ -3,123 +3,33 @@ import ReactDOM from "react-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
-
+import RenderSignInForm from './components/SignInForm';
+import RenderSignUpForm from './components/SignUpForm.jsx';
 import "./Apps/Login.css";
 
 export const App = () => {
   // React States
   const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [loginStatus, setLoginStatus] = useState(false);
   const [isSignIn, setIsSignIn] = useState(true);
 
-const handleSignInClick = () => {
-  setIsSignIn(true);};
-
-const handleSignUpClick = () => {
-  setIsSignIn(false);
-};
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1",
-      email: "aayildiz@email.com"
-    },
-    {
-      username: "user2",
-      password: "pass2",
-      email: "ntan@email.com"
-
-    }
-  ];
-  const errors = {
-    uname: "invalid username",
-    pass: "wrong password",
-    uemail: "wrong email"
+  const handleSignInClick = () => {
+    setIsSignIn(true);
+  };
+  const handleSignUpClick = () => {
+    setIsSignIn(false);
   };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
+//   if (Object.keys(errorMessages).length === 0) {
+//     setIsSubmitted(true);
+//   }
 
-  const formData = new FormData(event.target);
-  const unameOrEmail = formData.get("unameOrEmail");
-  const pass = formData.get("pass");
-
-  const userData = database.find(
-    (user) => user.username === unameOrEmail || user.email === unameOrEmail
-  );
-
-  if (userData) {
-    if (userData.password !== pass) {
-      setErrorMessages({ name: "pass", message: errors.pass });
-    } else {
-      setErrorMessages({});
-    }
-  } else {
-    if (unameOrEmail.includes("@")) {
-      setErrorMessages({ name: "unameOrEmail", message: errors.email });
-    } else {
-      setErrorMessages({ name: "unameOrEmail", message: errors.uname });
-    }
-  }
-  // Clear the form fields after submission
-  event.target.reset();
-
-  if (Object.keys(errorMessages).length === 0) {
-    setIsSubmitted(true);
-  }
-};
   // Generate JSX code for error message
   const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
+    errorMessages && errorMessages.name === name &&(
       <div className="error">{errorMessages.message}</div>
     );
 
-  // JSX code for login form
-  const renderSignInForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username or Email</label>
-          <input type="text" name="unameOrEmail" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-    </div>
-  );
-
-  const renderSignUpForm = (
-      <div className="form">
-        <form onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label>Username </label>
-            <input type="text" name="uname" required />
-            {renderErrorMessage("uname")}
-          </div>
-          <div className="input-container">
-              <label>E-mail </label>
-              <input type="text" name="uemail" required />
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input type="password" name="pass" required />
-            {renderErrorMessage("pass")}
-          </div>
-          <div className="button-container">
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
-      </div>
-    );
 
   return (
     <div className="app">
@@ -141,7 +51,7 @@ const handleSubmit = (event) => {
             </Row>
 
        </Container>
-        {isSubmitted ? <div>User is successfully logged in</div> : (isSignIn ? renderSignInForm : renderSignUpForm)}
+        {loginStatus ? <div>User is successfully logged in</div> : (isSignIn ? <RenderSignInForm loginStatus={loginStatus} /> : <RenderSignUpForm/>)}
       </div>
     </div>
   );
